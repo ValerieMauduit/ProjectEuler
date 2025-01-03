bc7d25af050366e1d9641aac07e5deb0fd46b1fd
Valerie Mauduit <valerie.mauduit@pricefx.com>
2025-01-03 16:42:53 +0100

    Initialisation

---
+++ Challenges/challenge_template.py
@@ -0,0 +1,19 @@
+# Challenge #
+# Description
+
+import os
+import sys
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+
+def my_func(data):
+    return data
+
+
+def run():
+    data = 42
+    solution = my_fun(data)
+    print(f'The solution is: {solution}')
+    return solution
